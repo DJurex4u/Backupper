5cb6667113818fe0477544225ad4bc633c9bb0d8
Ivan Budoš <budos@uhp-digital.com>
2019-11-22 13:07:36 +0100

    Route backuper/login is up

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
-use App\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -11,15 +11,15 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends  Controller{
 
     /**
-     * @Route("/login", name"login")
+     * @Route("/login", name="login")
     */
     public function login(Request $request, AuthenticationUtils $utils){
 
         $error = $utils->getLastAuthenticationError();
         $lastUsername = $utils->getLastUsername();
 
-        return $this->render(   'security/login.html.twig',
-                            [   'error' => '$error',
+        return $this->render(   'security/login.html.twig',[
+                                'error' => $error,
                                 'last_username'=>$lastUsername
         ]);
 
