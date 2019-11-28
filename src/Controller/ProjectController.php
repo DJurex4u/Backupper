8b273e71cd70e15837caa3477f1279d1beb534e1
Ivan Budoš <budos@uhp-digital.com>
2019-11-28 15:04:13 +0100

    Routes from ProjectController are valid, but not fully implemented

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -1,33 +1,131 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: UHP Digital
- * Date: 11/28/2019
- * Time: 9:40 AM
- */
 
 namespace App\Controller;
 
 
 use App\Entity\Project;
+use Doctrine\DBAL\Types\IntegerType;
+use Doctrine\DBAL\Types\TextType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectController extends AbstractController
 {
     /**
-     * @Route("/project/read, name="project_list")
+     * @Route("/project/list", name="project_list")
+     * @Method({"GET"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function listAction(){
-        $project = $this->getDoctrine()->getRepository(Project::class)->findAll;
-        return $this->render('project/read.html.twig', array('project => $project'));
+    public function listAction(Request $request)
+    {
+        $projects = $this->getDoctrine()->getRepository(Project::class)->findAll();
+
+        return $this->render('project/list.html.twig', array('projects' => $projects));
     }
 
     /**
-     * @Route("/project/read/{id}, name="project_read")
-    */
-    public function readAction($id){
-        $projects = $this->getDoctrine()->getRepository(Project::class)->find($id);
-        //return $this->
+     * @Route("/project/read/{id}", name="project_read")
+     * @Method({"GET"})
+     * @param Request $request
+     * @param int $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function readAction(Request $request, int $id)
+    {
+        $project = $this->getDoctrine()->getRepository(Project::class)->find($id);
+
+        if ($project) {
+            throw new \Exception('Cannot find project with given ID');
+        }
+
+        return $this->render('project/read.html.twig', array('project' => $project));
+
+    }
+
+    /**
+     * @Route("project/update/{id}", name="project_update")
+     * Method({"GET", "POST"})
+     * @param Request $request
+     * @param int $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function updateAction(Request $request,int $id)
+    {
+        $project = $this->getDoctrine()->getRepository(Project::class)->find($id);
+
+        if ($project) {
+            throw new \Exception('Cannot find project with given ID');
+        }
+
+        $form = $this->createFormBuilder($project)
+            ->add('name', TextType::class)
+            ->add('personInCharge', TextType::class)
+            ->add('keepAmount', IntegerType::class)
+            ->add('save', SubmitType::class, ['label' => 'Save'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($project);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('project/list.html.twig');
+        }
+
+        return $this->render('project/update.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("project/delete/{id}", name="project_delete")
+     * @Method({"GET"})
+     * @param Request $request
+     * @param int $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAction(Request $request, int $id)
+    {
+        $project = $this->getDoctrine()->getRepository(Project::class)->find($id);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($project);
+        //$entityManager->flush();
+
+        return $this->render('project/list.html.twig');
+
+    }
+
+    /**
+     * @Route("project/create", name="project_create")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function createAction(Request $request)
+    {
+        $project = new Project();
+        $form = $this->createFormBuilder($project)
+            ->add('name', TextType::class)
+            ->add('personInCharge', TextType::class)
+            ->add('keepAmount', IntegerType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($project);
+            $entityManager->flush();
+            return $this->redirectToRoute('project/list.html.twig');
+        }
+
+        return $this->render('project/create.html.twig', ['form' => $form->createView()]);
+
     }
 
 }
