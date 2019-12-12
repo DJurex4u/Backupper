1b6ff4e8b78c35e4dc0b6bfaa4015e356128d4f0
Ivan Budoš <budos@uhp-digital.com>
2019-12-12 16:03:57 +0100

    Armando the interface master

---
+++ src/Entity/Interfaces/IEncryptable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entity\Interfaces;
+
+interface IEncryptable
+{
+    public function getIv() :?string;
+    public function setIv() :self;
+    public function getPassword(): ?string;
+    public function setPassword(string $password): self;
+
+}
